485ead7e1c99d496ab0cea37e263ab90fedb4818
Gbenga Oni <gbengaoni@Gbengas-MacBook-Pro.local>
2019-12-21 17:18:33 +0100

    added testing#4

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -8,3 +8,8 @@ describe('Sample Test', () => {
       expect(false).toBe(false)
     })
   })
+  describe('Sample Test 3', () => {
+    it('should test that false === false', () => {
+      expect(false).toBe(false)
+    })
+  })
